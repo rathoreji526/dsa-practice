03920f44399f731e9c22713c7213aab3635b2026
Kamal Rathore <mr.rathoreji52@gmail.com>
2025-08-13 18:44:18 +0530

    Initial commit - added FindIndex.java

---
+++ src/binarySearch/findIndex.java
@@ -0,0 +1,22 @@
+package binarySearch;
+import java.util.*;
+
+public class findIndex {
+
+    public  static void main(String[] args) {
+        int[] arr = {1, 3, 5, 6, 10, 12, 17, 19, 22, 30};
+        int x = 19; // number to find in array
+        int si = 0, ei = arr.length - 1;
+
+        while (si <= ei) {
+            int mid = si + (ei - si) / 2;
+            if (arr[mid] == x) {
+                System.out.print(mid);
+                return;
+            }
+            if(arr[mid]<x)si = mid+1;
+            else ei = mid-1;
+        }
+        System.out.print(-1);
+    }
+}
