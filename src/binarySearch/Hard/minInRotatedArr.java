75387832ce3a9e5d1291f40da95a85699eb324fb
Kamal Rathore <mr.rathoreji52@gmail.com>
2025-08-16 08:18:58 +0530

    added new question find minimum in rotated sorted array

---
+++ src/binarySearch/Hard/minInRotatedArr.java
@@ -0,0 +1,18 @@
+package binarySearch.Hard;
+
+public class minInRotatedArr {
+    static int solution(int[] arr){
+        int start = 0 , end = arr.length-1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if(arr[mid]>arr[end]) start = mid+1;
+            else if(arr[mid]<arr[start]) end = mid;
+            else end--;
+        }
+        return arr[start];
+    }
+    public static void main(String[] args){
+        int[] arr = {2,2,2,0,1}; // before rotation {0, 1, 2 ,2 ,2};
+        System.out.println(solution(arr));
+    }
+}
