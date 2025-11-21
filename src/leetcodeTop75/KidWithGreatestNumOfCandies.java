58d221b7e8fe110895051aa7e855ca9621772766
Kamal Rathore <mr.rathoreji52@gmail.com>
2025-11-21 20:16:33 +0530

    changed solution class with solution_problemName

--- src/leetcodeTop75/KidWithGreatestNumOfCandies.java
+++ src/leetcodeTop75/KidWithGreatestNumOfCandies.java
@@ -3,7 +3,7 @@ package leetcodeTop75;
 import java.util.ArrayList;
 import java.util.List;
 
- class Solution {
+ class Solution_KidWithGreatestNumOfCandies {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         List<Boolean> res = new ArrayList<>(candies.length);
         int max = 0;
@@ -18,7 +18,7 @@ import java.util.List;
 public class KidWithGreatestNumOfCandies {
 
     public static void main(String[] args) {
-        Solution s = new Solution();
+        Solution_KidWithGreatestNumOfCandies s = new Solution_KidWithGreatestNumOfCandies();
         int[] candies  = {2,3,5,1,3};
         int extraCandies = 3;
 
