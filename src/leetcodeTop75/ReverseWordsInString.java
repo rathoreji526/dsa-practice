1dfb6647076ef1623618d4517e62cf6b4e21ed29
Kamal Rathore <mr.rathoreji52@gmail.com>
2025-11-21 20:15:25 +0530

    added new problem ReverseWordsInString

---
+++ src/leetcodeTop75/ReverseWordsInString.java
@@ -0,0 +1,32 @@
+package leetcodeTop75;
+
+/// Leetcode 151
+//A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+//
+//Return a string of the words in reverse order concatenated by a single space.
+//
+//Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+
+public class ReverseWordsInString {
+    public static void main(String[] args) {
+        Solution_ReverseWordsInString s1 = new Solution_ReverseWordsInString();
+        String res =  s1.reverseWords("the sky is blue");
+        System.out.println(res);
+    }
+}
+class Solution_ReverseWordsInString {
+    void reverse(String[] list){
+        int left = 0 , right = list.length-1;
+        while(left < right){
+            String temp = list[left];
+            list[left++] = list[right];
+            list[right--] = temp;
+        }
+    }
+    public String reverseWords(String s) {
+        String[] list = s.trim().split("\\s+");
+        reverse(list);
+        return String.join(" ", list);
+    }
+}
