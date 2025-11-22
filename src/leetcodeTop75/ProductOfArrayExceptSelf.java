f8d102e82fb2475f3ebdffefcc373f165e43a5cd
Kamal Rathore <mr.rathoreji52@gmail.com>
2025-11-22 20:07:05 +0530

    added exact problem in comment after reading that we can understand what exactly our problem says and its solution is below

--- src/leetcodeTop75/ProductOfArrayExceptSelf.java
+++ src/leetcodeTop75/ProductOfArrayExceptSelf.java
@@ -1,4 +1,10 @@
 package leetcodeTop75;
+///leetcode 238
+//Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+//
+//The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+//
+//You must write an algorithm that runs in O(n) time and without using the division operation.
 
 public class ProductOfArrayExceptSelf {
     public static void main(String[] args) {
